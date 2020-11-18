4b54c01a7beb2cadc3a80565884e754b5c8d07c7
CD-Mackay <connor.mackay@gmail.com>
2020-11-18 18:32:16 -0500

    attempt to get initial bar function working

---
+++ barscript.js
@@ -0,0 +1,7 @@
+
+function barFunction(height) {
+let bar = "<div class=\"bar\"> height </div>";
+$("#chart").prepend(bar);
+let height = document.getElementById("barValue").value;
+}
+barFunction();
